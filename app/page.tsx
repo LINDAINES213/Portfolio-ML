6ac3dee86de42ede5224172b844d8d7810eeb016
Diego Hernández <dahernandezsilvestre@gmail.com>
2025-08-03 20:21:08 -0600

    feat: test basePath for images

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src={`${basePath}/next.svg`}
           alt="Next.js logo"
           width={180}
           height={38}
